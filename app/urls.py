36500d5e28cdd9d0dfb9dd42ac40eb62d3776a1d
samhe2046 <hillongson@gmail.com>
2024-11-19 22:29:20 +0800

    Submit first batch files

---
+++ app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path("", views.Index, name="index"),
+    path("register", views.register, name="register"),
+    path("registration", views.Registration, name="registration"),
+    path("home", views.home, name="home"),
+    path("login", views.Loginview, name="login"),
+    path("votingpage/<int:pk>", views.Voting, name="votingpage"),
+    path("showques", views.show, name="show"),
+    path("logout", views.signout, name="logout"),
+    path("successfully", views.successfully, name="successfully"),
+    path("already", views.already, name="already"),
+    path('vote/results/<int:question_id>/', views.vote_results, name='vote_results')
+    
+]
